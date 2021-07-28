b03ed12e59ceebd28a212e6efffda3ce2f5d962e
siyavashtorabi <vashsiya81@gmail.com>
2021-07-28 03:58:35 -0500

    finished detail page

--- client/src/screens/SpecieCreate.jsx
+++ client/src/screens/SpecieCreate.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-
+import "./Forms.css"
 import { getAllRegions } from "../services/region";
 import { getAllEnvironments } from "../services/environments";
 import { Redirect } from "react-router-dom";
